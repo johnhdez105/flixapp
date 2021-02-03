78813ca825c47d2baf698e812574d6b719f7a1f6
johnhdez105 <johnhdez105@gmail.com>
2021-02-03 02:43:32 -0600

    Updated code to completion

--- flixapp/MovieCell.swift
+++ flixapp/MovieCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class MovieCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
-    @IBOutlet weak var posterLabel: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
