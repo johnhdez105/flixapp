efc4c3d964304e47d2ef3491398a3e8bed04ab7e
johnhdez105 <johnhdez105@gmail.com>
2021-02-11 17:24:49 -0600

    Update flix part 2

---
+++ flixapp/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixapp
+//
+//  Created by John H on 2/11/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
